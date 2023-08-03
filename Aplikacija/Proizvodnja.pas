8e4e63675d94b8a63b817bbc3d4762da707ba5f1
AleksaVukic <aleksavukic.lttm@gmail.com>
2023-08-03 13:42:41 +0200

    frmProizvodnja

---
+++ Aplikacija/Proizvodnja.pas
@@ -0,0 +1,24 @@
+unit Proizvodnja;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TfrmProizvodnja = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmProizvodnja: TfrmProizvodnja;
+
+implementation
+
+{$R *.fmx}
+
+end.
