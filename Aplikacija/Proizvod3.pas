677af65f11ab2069be8b71f856e20c2a09227f51
AleksaVukic <aleksavukic.lttm@gmail.com>
2023-08-30 11:03:59 +0200

    Prodaja

--- Aplikacija/Proizvod3.pas
+++ Aplikacija/Proizvod3.pas
@@ -43,6 +43,11 @@ type
     logo1: TImage;
     Visina: TRoundRect;
     Label8: TLabel;
+    RoundRect1: TRoundRect;
+    BACK: TLabel;
+    procedure RoundRect13Click(Sender: TObject);
+    procedure RoundRect12Click(Sender: TObject);
+    procedure RoundRect1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -55,5 +60,61 @@ var
 implementation
 
 {$R *.fmx}
+
+uses Prodaja;
+
+
+procedure TfrmProizvod3.RoundRect12Click(Sender: TObject);
+var
+RGBColor1: TAlphaColor;
+var
+RGBColor2: TAlphaColor;
+
+begin
+RGBColor1 := $FF709B8C;
+RGBColor2 := $FFE0E0E0;
+
+if RoundRect12.Fill.Color <> RGBColor1 then
+begin
+RoundRect12.Fill.Color := RGBColor1;
+end else
+begin
+RoundRect12.Fill.Color := RGBColor2;
+end;
+if RoundRect13.Fill.Color = RGBColor1 then
+begin
+  RoundRect13.Fill.Color:= RGBColor2;
+end;
+end;
+
+
+procedure TfrmProizvod3.RoundRect13Click(Sender: TObject);
+var
+RGBColor1: TAlphaColor;
+var
+RGBColor2: TAlphaColor;
+
+begin
+RGBColor1 := $FF709B8C;
+RGBColor2 := $FFE0E0E0;
+
+if RoundRect13.Fill.Color <> RGBColor1 then
+begin
+RoundRect13.Fill.Color := RGBColor1;
+end else
+begin
+RoundRect13.Fill.Color := RGBColor2;
+end;
+if RoundRect12.Fill.Color = RGBColor1 then
+begin
+  RoundRect12.Fill.Color:= RGBColor2;
+end;
+end;
+
+procedure TfrmProizvod3.RoundRect1Click(Sender: TObject);
+begin
+frmProdaja.show;
+self.hide;
+end;
 
 end.
