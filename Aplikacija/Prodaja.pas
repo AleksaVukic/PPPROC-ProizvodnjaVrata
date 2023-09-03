b5fab1b5c5b97d462f36a14b69630803f2a9d3e9
ognj4 <stepanovicognjenos@gmail.com>
2023-09-03 10:33:33 +0200

    new beggining

--- Aplikacija/Prodaja.pas
+++ Aplikacija/Prodaja.pas
@@ -5,7 +5,8 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects, FMX.Ani;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects, FMX.Ani, FMX.ListBox,
+  FMX.Layouts;
 
 type
   TfrmProdaja = class(TForm)
@@ -16,37 +17,13 @@ type
     Korpa: TButton;
     Back: TButton;
     PROIZVODI: TLabel;
-    Proizvod1: TRoundRect;
-    RoundRect1: TRoundRect;
-    p1: TLabel;
-    vrsta: TLabel;
-    pan_vrata: TImage;
-    Proizvod2: TRoundRect;
-    stakl_vrata: TImage;
-    RoundRect3: TRoundRect;
-    Label1: TLabel;
-    Label2: TLabel;
-    Proizvod3: TRoundRect;
-    stkl_delovi_vrata: TImage;
-    RoundRect4: TRoundRect;
-    Label3: TLabel;
-    Label4: TLabel;
-    Proizvod4: TRoundRect;
-    klizna_vrata: TImage;
-    RoundRect6: TRoundRect;
-    Label5: TLabel;
-    Label6: TLabel;
-    RoundRect2: TRoundRect;
-    Label7: TLabel;
-    Vise1: TRoundRect;
-    Label8: TLabel;
-    Vise2: TRoundRect;
-    Label9: TLabel;
-    Vise3: TRoundRect;
-    Label10: TLabel;
-    Vise4: TRoundRect;
-    Label11: TLabel;
     Image1: TImage;
+    ListBox1: TListBox;
+    ListBoxItem1: TListBoxItem;
+    ListBoxItem2: TListBoxItem;
+    ListBoxItem3: TListBoxItem;
+    ListBoxItem4: TListBoxItem;
+    ListBoxItem5: TListBoxItem;
     procedure KorpaClick(Sender: TObject);
     procedure Vise1Click(Sender: TObject);
     procedure Vise2Click(Sender: TObject);
